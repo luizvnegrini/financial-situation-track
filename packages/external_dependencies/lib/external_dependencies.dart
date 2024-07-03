f0f61b53d166f31f7f102800ef11687780be084b
Luiz Negrini <luizv.negrini@outlook.com>
2024-07-03 07:50:49 -0300

    chore: setup design_system and external_dependencies packages

---
+++ packages/external_dependencies/lib/external_dependencies.dart
@@ -0,0 +1,7 @@
+library external_dependencies;
+
+export 'package:equatable/equatable.dart';
+export 'package:flutter_hooks/flutter_hooks.dart';
+export 'package:go_router/go_router.dart';
+export 'package:hooks_riverpod/hooks_riverpod.dart';
+export 'package:stack_trace/stack_trace.dart';
